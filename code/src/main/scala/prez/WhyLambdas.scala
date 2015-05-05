92c8595e7d5949720c7b9d3b3ef12527c803dd72
Krzysztof Romanowski <romanowski.kr@gmail.com>
2015-05-05 16:13:35 +0200

    basic skatch

---
+++ code/src/main/scala/prez/WhyLambdas.scala
@@ -0,0 +1,18 @@
+package prez
+
+case class Ala(i: Int)
+
+/**
+ * Author: Krzysztof Romanowski
+ */
+object WhyLambdas extends App {
+
+  def test(ala: Int): Unit ={
+    val ola: Int = 123
+
+    val list = List(1, 2, 3)
+    println("ala")
+  }
+
+  test(1)
+}
